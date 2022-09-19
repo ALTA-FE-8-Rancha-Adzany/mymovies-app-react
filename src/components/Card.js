5e5b5d3485b98d34e86d5c9e942d6010813d9842
ray34-cyber <rancha.adzany1@gmail.com>
2022-09-20 05:02:40 +0800

    menambahakan tugas Release 2 (Fetch API)

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@ export default class Card extends Component {
   render() {
     return (
       <div className="card" style={{ width: "18rem" }}>
-        <img src={this.props.src} className="card-img-top" alt="..." />
+        <img src={`https:image.tmdb.org/t/p/w500/${this.props.data.poster_path}`} className="card-img-top" alt="test" />
         <div className="card-body d-flex flex-column">
-          <h5 className="card-title">{this.props.title}</h5>
-          <p className="card-text">{this.props.summary.substring(0, 50)}</p>
+          <h5 className="card-title">{this.props.data.title}</h5>
+          <p className="card-text">{this.props.data.overview.substring(0, 100)}</p>
           <a href="#" className="btn btn-primary">
             Detail page
           </a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,30 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             My movies
           </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item mx-3">
+                <a className="nav-link active" aria-current="page" href="#">
                   Now Playing
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#">
                   Favorites
                 </a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
             </form>
